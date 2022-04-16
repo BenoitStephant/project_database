f2388179fb2d5c6aeeb81664f277ee2b88db4e97
Goruku <DAMAL79@ulaval.ca>
2022-04-16 11:43:03 -0400

    Remove delimiters

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -135,8 +135,7 @@ CREATE TABLE IF NOT EXISTS Match_log (
         ON DELETE CASCADE
 );
 
-DELIMITER //
-DROP TRIGGER IF EXISTS category_item_propagator //
+DROP TRIGGER IF EXISTS category_item_propagator;
 CREATE TRIGGER category_item_propagator
     AFTER INSERT ON Category_items
     FOR EACH ROW
@@ -144,17 +143,17 @@ BEGIN
     INSERT INTO Elo
     SELECT NULL, NEW.item_id, NEW.category_id, Cc.concept_id, 1500
     FROM (SELECT Cc.concept_id FROM Category_concepts Cc WHERE Cc.category_id = NEW.category_id) as Cc;
-END //
+END;
 
-DROP TRIGGER IF EXISTS category_item_delinker //
+DROP TRIGGER IF EXISTS category_item_delinker;
 CREATE TRIGGER category_item_delinker
     AFTER DELETE ON Category_items
     FOR EACH ROW
 BEGIN
     DELETE FROM Elo WHERE Elo.item_id = OLD.item_id AND Elo.category_id = OLD.category_id;
-END //
+END;
 
-DROP TRIGGER IF EXISTS category_concept_propagator //
+DROP TRIGGER IF EXISTS category_concept_propagator;
 CREATE TRIGGER category_concept_propagator
     AFTER INSERT ON Category_concepts
     FOR EACH ROW
@@ -162,13 +161,12 @@ BEGIN
     INSERT INTO Elo
     SELECT  NULL, Ci.item_id, NEW.category_id, NEW.concept_id, 1500
     FROM (SELECT Ci.item_id FROM Category_items Ci WHERE  Ci.category_id = NEW.category_id) as Ci;
-END //
+END;
 
-DROP TRIGGER IF EXISTS category_concept_delinker //
+DROP TRIGGER IF EXISTS category_concept_delinker;
 CREATE TRIGGER category_concept_delinker
     AFTER DELETE ON Category_concepts
     FOR EACH ROW
 BEGIN
     DELETE FROM Elo WHERE Elo.category_id = OLD.category_id AND Elo.concept_id = OLD.concept_id;
-END //
-DELIMITER ;
+END;
