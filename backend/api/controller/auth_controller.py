6682b52a3c00a29fb654357fb70457bdeef59a63
Benoit Stephant <benoit.stephant@gmail.com>
2022-04-18 14:18:37 +0200

    [FIX]: login and register

--- backend/api/controller/auth_controller.py
+++ backend/api/controller/auth_controller.py
@@ -9,16 +9,31 @@ class AuthController(Resource):
     parser.add_argument("password", type=str, required=True, help="Can't be empty")
 
     def register():
-        print()
         data = AuthController.parser.parse_args()
         try:
             db = connect_db()
             cursor = get_cursor(db)
-            cursor.execute("SELECT * FROM User WHERE username=%s", (data.username,))
+            cursor.callproc("get_user_by_name", args=(data.username,))
             result = cursor.fetchall()
             if result:
                 return {"message": "User with this username is already register."}, 409
             return AuthService.register(data.username, data.password, db, cursor)
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            db.close()
+
+    def login():
+        data = AuthController.parser.parse_args()
+        try:
+            db = connect_db()
+            cursor = get_cursor(db)
+            cursor.execute("call get_user_by_name(%s)", (data.username,))
+            result = cursor.fetchall()
+            if not result:
+                return {"message": "Bad combinaison of password and username."}, 401
+            return AuthService.login(data.username, data.password, result, db, cursor)
         except Exception as e:
             print(e)
         finally:
--- backend/api/service/auth_service.py
+++ backend/api/service/auth_service.py
@@ -1,24 +1,39 @@
+import uuid
 from database.database import connect_db, get_cursor
+from flask_jwt_extended import create_access_token, jwt_required
+from flask import jsonify
 
 import hashlib
-import time
 
 
 class AuthService:
     def register(username, password, db, cursor):
-        cursor.execute("SELECT * FROM User WHERE username=%s", (username,))
-        result = cursor.fetchall()
-        if result:
-            return {"message": "User with this username is already register."}, 409
         hash_password = hashlib.md5(password.encode("utf-8")).hexdigest()
-        date_now = time.strftime("%Y-%m-%d %H:%M:%S")
+        user_id = str(uuid.uuid4())
         cursor.execute(
             "INSERT INTO User (id, username, password) VALUES (%s, %s, %s)",
             (
-                0,
+                user_id,
                 username,
                 hash_password,
             ),
         )
         db.commit()
         return {"message": "User successfully created."}, 201
+
+    def login(username, password, user, db, cursor):
+        hash_password_request = hashlib.md5(password.encode("utf-8")).hexdigest()
+        hash_password_db = user[0][2]
+        if hash_password_request != hash_password_db:
+            return {"message": "Bad combinaison of password and username."}, 401
+        user = {
+            "id": user[0][0],
+            "username": user[0][1],
+            "match_nb": str(user[0][3]),
+            "average": str(user[0][4]),
+            "favorite_category_id": user[0][5],
+            "created_at": user[0][6],
+            "updated_at": user[0][7],
+        }
+        access_token = create_access_token({"id": user["id"], "username": user["username"]})
+        return jsonify(user=user, access_token=access_token)
