962e0b2e9cc5a40f93516be4568351f0634221ce
Benoit Stephant <benoit.stephant@gmail.com>
2022-04-19 15:06:46 +0200

    [FIX]: return value of GET category

--- backend/api/service/category_service.py
+++ backend/api/service/category_service.py
@@ -1,3 +1,6 @@
+from flask import jsonify
+
+
 class CategoryService:
     def create(title, description, image_url, db, cursor):
         cursor.callproc("create_category", args=(title, description, image_url))
@@ -5,7 +8,15 @@ class CategoryService:
 
     def find_all(db, cursor):
         cursor.execute("SELECT * FROM Category");
-        concepts = []
+        categories = []
         for row in cursor.fetchall():
-            concepts.append({"id": str(row[0]), "name": row[1], "created_at": str(row[2])})
-        return concepts
+            categories.append(
+                {
+                    "id": row[0],
+                    "name": row[1],
+                    "description": row[2],
+                    "image_url": row[3],
+                    "created_at": row[4],
+                    "updated_at": row[5]
+                })
+        return jsonify(categories=categories)
